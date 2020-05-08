b9a089e225575f8ad106ef9d9e67e77dc75068fe
jothamc <charles.jotham@gmail.com>
2020-05-08 13:34:30 +0100

    remove styles.css

--- CarSupply/settings.py
+++ CarSupply/settings.py
@@ -143,9 +143,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, "static"),
+# ]
 
 
 AUTH_USER_MODEL = 'users.User'
