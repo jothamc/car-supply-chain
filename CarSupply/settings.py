28216dfb40a90b283629de13adb5ed4e5691c0cc
jothamc <charles.jotham@gmail.com>
2020-05-08 14:16:18 +0100

    add dj_database_url for heroku db

--- CarSupply/settings.py
+++ CarSupply/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -101,6 +103,10 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
